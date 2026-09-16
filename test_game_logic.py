f2f9f41c1dc083ad47fc383d342ca29195bca60b
Dan Knights <danknights@gmail.com>
2026-09-16 14:10:00 -0500

    No feature yet

--- test_game_logic.py
+++ test_game_logic.py
@@ -15,8 +15,75 @@ class GameLogicTests(unittest.TestCase):
         moved = move_tile(board, 15, 4)
         self.assertEqual(moved, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
 
-        self.assertIsNone(move_tile(board, 12, 4))
+        self.assertIsNone(move_tile(board, 9, 4))
         self.assertEqual(board[-2:], [16, 15])
+
+    def test_pushes_tiles_horizontally_toward_a_blank_on_the_right(self):
+        board = [1, 2, 9, 3, 4, 5, 6, 7, 8]
+
+        moved = move_tile(board, 0, 3)
+
+        self.assertEqual(moved, [9, 1, 2, 3, 4, 5, 6, 7, 8])
+
+    def test_pushes_tiles_horizontally_toward_a_blank_on_the_left(self):
+        board = [9, 1, 2, 3, 4, 5, 6, 7, 8]
+
+        moved = move_tile(board, 2, 3)
+
+        self.assertEqual(moved, [1, 2, 9, 3, 4, 5, 6, 7, 8])
+
+    def test_pushes_tiles_vertically_toward_a_blank_below(self):
+        board = [1, 2, 3, 4, 5, 6, 7, 9, 8]
+
+        moved = move_tile(board, 1, 3)
+
+        self.assertEqual(moved, [1, 9, 3, 4, 2, 6, 7, 5, 8])
+
+    def test_pushes_tiles_vertically_toward_a_blank_above(self):
+        board = [1, 9, 3, 4, 2, 6, 7, 5, 8]
+
+        moved = move_tile(board, 7, 3)
+
+        self.assertEqual(moved, [1, 2, 3, 4, 5, 6, 7, 9, 8])
+
+    def test_four_by_four_push_can_shift_three_tiles(self):
+        board = [1, 2, 3, 16, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 4]
+
+        moved = move_tile(board, 15, 4)
+
+        self.assertEqual(
+            moved,
+            [1, 2, 3, 8, 5, 6, 7, 12, 9, 10, 11, 4, 13, 14, 15, 16],
+        )
+
+    def test_line_moves_work_for_the_2_by_2_and_4_by_4_boards(self):
+        boards = {
+            2: ([1, 4, 3, 2], 3, [1, 2, 3, 4]),
+            4: (
+                [1, 2, 3, 4, 5, 6, 16, 8, 9, 10, 7, 12, 13, 14, 11, 15],
+                14,
+                [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 15],
+            ),
+        }
+        for size, (board, index, expected) in boards.items():
+            with self.subTest(size=size):
+                self.assertEqual(move_tile(board, index, size), expected)
+
+    def test_non_aligned_tile_returns_none_without_mutating_the_board(self):
+        board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        moved = move_tile(board, 0, 3)
+
+        self.assertIsNone(moved)
+        self.assertEqual(board, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+    def test_valid_line_move_does_not_mutate_the_input_board(self):
+        board = [1, 2, 9, 3, 4, 5, 6, 7, 8]
+
+        moved = move_tile(board, 0, 3)
+
+        self.assertEqual(moved, [9, 1, 2, 3, 4, 5, 6, 7, 8])
+        self.assertEqual(board, [1, 2, 9, 3, 4, 5, 6, 7, 8])
 
     def test_solvability_uses_the_board_width(self):
         self.assertTrue(is_solvable(solved_board(3), 3))
